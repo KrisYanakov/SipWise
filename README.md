058b9f5ed72644b05f734374a6a096921cb36b9f
KrisYanakov <kyanakov@tu-sofia.bg>
2025-03-04 21:28:34 +0200

    Last praktikum href features

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 "# SipWise" 
-A modern functional Drink & Drive - inspired website for a local service.
-
--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //alert("ASD")
+var accounts = []
 
+function getAccountInfo(){
+    let email = document.getElementById("inputEmail").value;
+    let password = document.getElementById("inputPassword").value;
+
+    if(email != '' && password != ''){
+        alert("Your email is: "+ email + "\nNew Account created")
+        accounts.push([email, password])
+    }
+}
+
+function goToSite(){
+    window.location.href = "https://github.com/KrisYanakov/SipWise";
+}
+
+function info(){
+    for(let i = 0; i < accounts.length; i ++){
+        console.log("Username: " + accounts[i][0] + " Password: " + accounts[i][1]+"\n")
+    }
+}
